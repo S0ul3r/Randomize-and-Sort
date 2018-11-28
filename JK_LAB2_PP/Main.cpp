e71932ad94473b78e823dc06785402da87d52081
Jakub <jakubkulewicz05@gmail.com>
2018-11-28 11:22:18 +0100

    Choose ascending or descending sort
    User can choose if the numbers should be sorted ascending or descending

--- JK_LAB2_PP/Main.cpp
+++ JK_LAB2_PP/Main.cpp
@@ -3,25 +3,35 @@
 int main()
 {
 	int size, start, end;
+	bool type;
 	printf("Give a number how large the table should be: ");
 	scanf_s("%d", &size);
-	printf("Enter the numerical range of random, starting number: ");
+	printf("Enter the starting number for random value generator: ");
 	scanf_s("%d", &start);
 	printf("End: ");
 	scanf_s("%d", &end);
+	printf("\n");
 	int* arr = new int[size];
 	randomizer(arr, size, start, end);
 	for (int i = 0; i < size; i++)printf("%d, ", arr[i]);
+	printf("\n\nChoose if the table should be ascending (press 0) or descending (press 1): ");
+	scanf_s("%d", &type);
 	printf("\n\n");
-	printf("Press anything to sort an array\n\n");
-	_getch();
 
-	sort(arr, size);
-	for (int i = 0; i < size; i++)printf("%d, ", arr[i]);
+	if (type == 0) {
+		sortasc(arr, size);
+		for (int i = 0; i < size; i++)printf("%d, ", arr[i]);
+	}
+	else if (type == 1) {
+		sortdesc(arr, size);
+		for (int i = 0; i < size; i++)printf("%d, ", arr[i]);
+	}
+	else {
+		printf("You put the wrong value");
+	}
 	delete[] arr;
 	printf("\n\nPress anything to close program");
 	_getch();
 	_getch();
-
 	return 0;
 }
